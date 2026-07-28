83d6b0d5a5220bcdca333ee2024695f313210573
Jaspher28 <jasphersujin2020@gmail.com>
2026-07-28 22:54:21 +0530

    updated contact.ts

--- .vercel/output/_functions/chunks/contact_v5auihzf.mjs
+++ .vercel/output/_functions/chunks/contact_v5auihzf.mjs
@@ -1,27 +1,23 @@
-import type { APIRoute } from "astro";
-import { Resend } from "resend";
+import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY!);
-
-export const POST: APIRoute = async ({ request }) => {
+const resend = new Resend(process.env.RESEND_API_KEY);
+const POST = async ({ request }) => {
   try {
     const { name, email, subject, message } = await request.json();
-
     if (!name || !email || !subject || !message) {
       return new Response(
         JSON.stringify({
           success: false,
-          message: "All fields are required.",
+          message: "All fields are required."
         }),
         {
-          status: 400,
+          status: 400
         }
       );
     }
-
     await resend.emails.send({
       from: "Portfolio <onboarding@resend.dev>",
-      to: process.env.CONTACT_EMAIL!,
+      to: process.env.CONTACT_EMAIL,
       replyTo: email,
       subject: `Portfolio Contact • ${subject}`,
       html: `
@@ -38,28 +34,35 @@ export const POST: APIRoute = async ({ request }) => {
 
           <p>${message.replace(/\n/g, "<br/>")}</p>
         </div>
-      `,
+      `
     });
-
     return new Response(
       JSON.stringify({
-        success: true,
+        success: true
       }),
       {
-        status: 200,
+        status: 200
       }
     );
   } catch (err) {
     console.error(err);
-
     return new Response(
       JSON.stringify({
         success: false,
-        message: "Unable to send email.",
+        message: "Unable to send email."
       }),
       {
-        status: 500,
+        status: 500
       }
     );
   }
 };
+
+const _page = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+  __proto__: null,
+  POST
+}, Symbol.toStringTag, { value: 'Module' }));
+
+const page = () => _page;
+
+export { page };
